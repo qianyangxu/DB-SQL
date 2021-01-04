3fd458ef77467b3979599ba8be32dbc3724fe91b
1091419378@qq.com <beibei1992629>
2021-01-04 15:24:58 +0800

    根据字典的value获取对应的label

--- oracle/function/fun_get_label_by_value.sql
+++ oracle/function/fun_get_label_by_value.sql
@@ -1,7 +1,8 @@
+-- 按value获取字段表对应的label
 create or replace FUNCTION fun_get_label_by_value (
-	p_str IN VARCHAR,
-	p_dict_type IN VARCHAR,
-	p_split_str IN VARCHAR
+	p_str IN VARCHAR, -- 待切割字符串
+	p_dict_type IN VARCHAR, -- 数据库字典类型
+	p_split_str IN VARCHAR -- 切割符
 ) RETURN VARCHAR2 IS RESULT VARCHAR2 (1024) ;
 dictLabel VARCHAR2(50);
 BEGIN
